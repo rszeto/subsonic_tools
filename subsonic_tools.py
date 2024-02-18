ed2f9e89b076d5582e470489da5f373d1f60bfcd
Ryan Szeto <contact@ryanszeto.com>
2024-02-18 14:04:11 -0500

    Upgrade to Python 3; fix authentication; add export playlists fn

--- subsonic_tools.py
+++ subsonic_tools.py
@@ -24,24 +24,34 @@ subsonic_apiVersion = '1.8.0'
 
 def create_subsonic_call_fn(subsonic_url, subsonic_user, subsonic_password, verbose):
 
-    def subsonic_call(command, params = []):
+    def subsonic_call(command, params = {}):
         # Construct the REST call
-        url = '%s/rest/%s.view?v=%s&c=%s&%s' % (subsonic_url, command, subsonic_apiVersion,
-                                                subsonic_client, urllib.urlencode(params, True))
+        url = '%s/rest/%s.view?v=%s&c=%s&%s&%s' % (
+            subsonic_url, 
+            command, 
+            subsonic_apiVersion,
+            subsonic_client,
+            urllib.parse.urlencode({
+                'u': subsonic_user, 
+                'p': subsonic_password
+            }),
+            urllib.parse.urlencode(params, True),
+        )
         if verbose:
             print('Accessing URL: %s' % url)
 
         # Send the request
-        response = requests.get(url, verify=True, auth=(subsonic_user, subsonic_password))
+        response = requests.get(url)
+        response.encoding = 'UTF-8'
         if verbose:
             print('Received response:')
-            print(response.text.encode('utf-8'))
+            print(response.text)
 
         if response.status_code != 200:
             raise Exception('Subsonic REST API returned status code %s' % response.status_code)
 
         # Parse the response (obtain the content and any errors)
-        root = ET.fromstring(response.text.encode('utf-8'))
+        root = ET.fromstring(response.text)
         error = root.find('{%(ns)s}error' % {'ns': subsonic_namespace})
         
         if error is not None:
@@ -73,7 +83,7 @@ def sort_playlists(args):
         playlist = subsonic_call('getPlaylist', {'id': playlist.get('id')})
 
         playlist_title = playlist.get('name')
-        print('Processing playlist "%s"' % playlist_title.encode('utf-8'))
+        print('Processing playlist "%s"' % playlist_title)
 
         song_titles = []
         song_ids = []
@@ -103,7 +113,7 @@ def sort_playlists(args):
             'songIdToAdd': sorted_song_ids
         })
 
-    print 'Done.'    
+    print('Done.')
 
 def argsort(seq, key=None):
     """Adapted from https://stackoverflow.com/a/6979121"""
@@ -117,6 +127,38 @@ def index_select(seq, indexes):
         ret.append(seq[i])
     return ret
 
+def export_playlists(args):
+    # Prepare with data from config file
+    with open(args.config_path, 'r') as f:
+        config = json.load(f)
+    music_root = config['music_root']
+    subsonic_call = create_subsonic_call_fn(config['server_url'], config['user'],
+                                            config['password'], args.verbose)
+    
+    # Get list of playlists
+    playlists = subsonic_call('getPlaylists', {'username': 'szetor'})
+    for playlist in playlists.iter('{%(ns)s}playlist' % {'ns': subsonic_namespace}):
+    
+        # Get the songs in the current playlist
+        playlist_id = playlist.get('id')
+        playlist = subsonic_call('getPlaylist', {'id': playlist.get('id')})
+
+        playlist_title = playlist.get('name')
+        print('Processing playlist "%s"' % playlist_title)
+
+        song_path_format = '%(artist)s/%(album)s/%(title)s.%(suffix)s'
+        full_paths = []
+        for entry in playlist.iter('{%(ns)s}entry' % {'ns': subsonic_namespace }):
+            song_id = entry.get('id')
+            song_info = subsonic_call('getSong', {'id': song_id})
+            song_path = song_path_format % song_info.attrib
+            full_paths.append('%s/%s' % (music_root, song_path))
+        
+        with open('%s/%s.m3u' % (args.export_root, playlist_title), 'w') as f:
+            f.write('\n'.join(full_paths))
+    
+    print('Done.')
+        
 def main():
     print('Licensed under the GPLv2 (http://www.gnu.org/licenses/gpl-2.0.html). Distributed on an'
         ' "AS IS" basis without warranties or conditions of any kind, either express or implied.\n')
@@ -130,8 +172,14 @@ def main():
                                        help='Path to the configuration file')
     parser_sort_playlists.set_defaults(func=sort_playlists)
 
+    parser_export_playlists = subparsers.add_parser('export_playlists')
+    parser_export_playlists.add_argument('export_root', type=str, help='Path to save playlists to')
+    parser_export_playlists.add_argument('--config_path', type=str, default='config.json',
+                                       help='Path to the configuration file')
+    parser_export_playlists.set_defaults(func=export_playlists)
+
     args = parser_parent.parse_args()
-    args.func(args);
+    args.func(args)
 
 if __name__ == '__main__':
     main()
